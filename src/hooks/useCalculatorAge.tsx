f9d55a0d33aec930fd4bd0c7e5063547f1ffa8b6
Thằng Dev Quèn <140314115+devquen2023@users.noreply.github.com>
2023-07-25 22:22:07 +0700

    Update useCalculatorAge.tsx

--- src/hooks/useCalculatorAge.tsx
+++ src/hooks/useCalculatorAge.tsx
@@ -33,7 +33,7 @@ const calculateAgeFromYear = (year: number) => {
           return false;
         }
   
-        if (birthYear > 113 && birthYear < 1910) {
+        if (birthYear > 114 && birthYear <= 1910) {
           setErrorMsg("No one can have an age or birth year like that.");
           setAge(null);
           e.preventDefault();
