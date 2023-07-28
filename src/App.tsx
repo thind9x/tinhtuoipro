1cc6fd5403f8065843f1cc6e36b61f19cfcce9c3
thind9x <developer@phuongmychi.vn>
2023-07-28 17:02:56 +0700

    feat: add license

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import "./App.css"
-import { useLanguage, useCalculatorAge } from "./hooks"
-import { DatePicker } from "./components"
+/*
+  Copyright 2023  Phuong My Chi Entertainment Co.,Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+
+import "./App.css";
+import { useCalculatorAge, useLanguage } from "./hooks";
 export default function App() {
 
   const { memoizedGetMessage, isVietNam, onChangeLang, } = useLanguage()
-  const { age, errorMsg, handleInputChange, memoizedHandleSubmit, onChangeToRealAge, isRealAge } = useCalculatorAge();
+  const { age, errorMsg, handleInputChange, memoizedHandleSubmit, isRealAge } = useCalculatorAge();
 
   return (
     <div style={{ marginTop: '100px' }}>
--- src/hooks/useCalculatorAge.tsx
+++ src/hooks/useCalculatorAge.tsx
@@ -1,94 +1,83 @@
-import { useState, useMemo, useCallback } from "react";
+/*
+ Copyright 2023 Phuong My Chi Entertainment Co.,Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import { useState, useCallback, useMemo } from 'react';
 import useLanguage from "./useLanguage";
 
+const useCalculatorAge = () => {
+  const [age, setAge] = useState<number>();
+  const [birthYear, setBirthYear] = useState<number>();
+  const [errorMsg, setErrorMsg] = useState("");
+  const { memoizedGetMessage } = useLanguage();
+  const [isRealAge, setisRealAge] = useState<boolean>(false);
 
-
-  const useCalculatorAge = () => {
-    const [age, setAge] = useState<number>();
-    const [birthYear, setBirthYear] = useState<number>();
-    const [errorMsg, setErrorMsg] = useState("");
-    const {memoizedGetMessage} = useLanguage()
-    const [isRealAge,setisRealAge] = useState<boolean>(false)
-    const calculateAgeFromYear = (year: number) => {
-      const currentYear = new Date().getFullYear();
-      return currentYear - year;
-    };
-
-    const memoizedCalculateAgeFromYear = useMemo(() => calculateAgeFromYear, []);
-
-    const memoizedHandleSubmit = useCallback(
-      (e: any) => {
-        const currentYear = new Date().getFullYear();
-          // if(birthYear===null){
-          //   setErrorMsg("Please enter your  birth year or age");
-          //   e.preventDefault();
-          //   return false;
-
-          // }
-        
-          if (!Number.isInteger(birthYear) )  {
-            setErrorMsg("Please enter a valid birth year or age.");
-            setAge(null);
-            e.preventDefault();
-            return false;
-          }
-    
-          if (birthYear > currentYear) {
-            setErrorMsg("Birth year cannot be greater than the current year.");
-            setAge(null);
-            e.preventDefault();
-            return false;
-          }
-    
-          if (birthYear > 114 && birthYear <= 1908) {
-            setErrorMsg("No one can have an age like that.");
-            setAge(null);
-            e.preventDefault();
-            return false;
-          }
-    
-          setErrorMsg("");
-          setAge(memoizedCalculateAgeFromYear(birthYear));
-          e.preventDefault();
-      },
-    
-      [birthYear, memoizedCalculateAgeFromYear],
-    );
-  
-    const handleInputChange2 = (e: any) => {
-      const inputValue = e?.target.value;
-      if (/^\d*$/.test(inputValue)) {
-        setBirthYear(parseInt(inputValue));
-        setErrorMsg("");
-      } else {
-        setErrorMsg(memoizedGetMessage("Please enter a positive integer."));
-        setAge(null);
-      }
-    };
-    
-    const handleInputChange = useCallback((e:any)=>{
-      const inputValue = e?.target.value;
-      if (/^\d*$/.test(inputValue)) {
-        setBirthYear(parseInt(inputValue));
-        setErrorMsg("");
-      } else {
-        setErrorMsg(memoizedGetMessage("Please enter a positive integer."));
-        setAge(null);
-      }
-    },[])
- 
-    const onChangeToRealAge = (e) =>{
-      setisRealAge(!isRealAge);
-    }
- 
-    return {
-      handleInputChange,
-      memoizedHandleSubmit,
-      errorMsg,
-      age,
-      onChangeToRealAge,
-      isRealAge
-    };
+  const calculateAgeFromYear = (year: number) => {
+    const currentYear = new Date().getFullYear();
+    return currentYear - year;
   };
-  
-  export default useCalculatorAge;
+
+  const memoizedCalculateAgeFromYear = useMemo(() => calculateAgeFromYear, []);
+
+  const handleInputChange = useCallback((e: any) => {
+    const inputValue = e?.target.value;
+    if (/^\d*$/.test(inputValue)) {
+      const year = parseInt(inputValue);
+      setBirthYear(year);
+      setErrorMsg("");
+    } else {
+      setErrorMsg(memoizedGetMessage("Please enter a positive integer."));
+      setAge(undefined);
+    }
+  }, [memoizedGetMessage]);
+
+  const memoizedHandleSubmit = useCallback( async (e: any) => {
+    e.preventDefault();
+    const currentYear = new Date().getFullYear();
+
+    if (!Number.isInteger(birthYear)) {
+      setErrorMsg("Please enter a valid birth year or age.");
+      return;
+    }
+
+    if (birthYear > currentYear) {
+      setErrorMsg("Birth year cannot be greater than the current year.");
+      return;
+    }
+
+    if (birthYear > 114 && birthYear <= 1908) {
+      setErrorMsg("No one can have an age like that.");
+      return;
+    }
+
+    setErrorMsg("");
+    setAge(memoizedCalculateAgeFromYear(birthYear));
+  }, [birthYear, memoizedCalculateAgeFromYear]);
+
+  const onChangeToRealAge = (e) => {
+    setisRealAge(!isRealAge);
+  };
+
+  return {
+    handleInputChange,
+    memoizedHandleSubmit,
+    errorMsg,
+    age,
+    onChangeToRealAge,
+    isRealAge,
+  };
+};
+
+export default useCalculatorAge;
